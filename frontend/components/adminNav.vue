9b499c9efeec8d13ae58273395bf65d2b359d27f
macsakarn <macsakarn@gmail.com>
2021-04-04 04:43:16 +0700

    Done UI Login & nav, side nav

---
+++ frontend/components/adminNav.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="allNavBar">
+    <nav class="flex justify-between bg-white items-center max-w-full h-10">
+      <div class="titleName ml-64" v-if="isLogin">
+        <p class="text-lg">
+          Welcome Admin : <span style="color: #ffc700">{{ username }}</span>
+        </p>
+      </div>
+      <div
+        class="exit order-last mr-16 flex-none cursor-pointer"
+        v-if="isLogin"
+      >
+        <img src="~/assets/ADMIN/logout_black_24dp.svg" alt="" />
+      </div>
+    </nav>
+    <aside class="bg-gray-900 w-56 fixed inset-y-0 text-white">
+      <div class="hero mt-48 text-gray-500">
+        <div class="bth_home text-center my-10 cursor-pointer" v-if="isLogin">
+          <img src="~/assets/ADMIN/home_white_24dp.svg" class="inline w-8" />
+          <span class="align-bottom">Home</span>
+        </div>
+        <div class="bth_book text-center my-10 cursor-pointer" v-if="isLogin">
+          <img
+            src="~/assets/ADMIN/menu_book_white_24dp.svg"
+            class="inline w-8"
+          />
+          <span class="align-bottom">Book</span>
+        </div>
+        <div class="bth_order text-center my-10 cursor-pointer" v-if="isLogin">
+          <img
+            src="~/assets/ADMIN/list_alt_white_24dp.svg"
+            class="inline w-8"
+          />
+          <span class="align-bottom">Order</span>
+        </div>
+      </div>
+    </aside>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLogin: false,
+      username: "Sakarn Bantadjun",
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  background-color: #dbdbdb;
+}
+.bth_home:hover span {
+  color: #fff;
+}
+.bth_book:hover span {
+  color: #fff;
+}
+.bth_order:hover span {
+  color: #fff;
+}
+</style>
--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,10 +1,58 @@
 <template>
-  <div class="">This is admin</div>
+  <section>
+    <adminNav />
+    <section
+      class="flex justify-center mt-8 w-4/5 bg-white"
+      style="margin-left: 17%"
+    >
+      <form class="bg-white px-8 pt-6 pb-64 mb-4 mt-16 w-2/4">
+        <div class="mb-8">
+          <label
+            class="block text-gray-700 text-lg font-bold mb-2"
+            for="username"
+          >
+            Username
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-lg font-bold mb-2">
+            Password
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            placeholder="******************"
+            :class="{ 'border-red-500': errorPassword }"
+          />
+          <p class="text-red-500 text-xs italic" v-show="errorPassword">
+            Please choose a password.
+          </p>
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:b g-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </section>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      errorPassword: false,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style >
 </style>
