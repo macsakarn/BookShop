eb18d0e6b8e82aa03d39ca52ac813a2da90bbe0d
Sakarn <macsakarn@gmail.com>
2021-04-16 21:42:06 +0700

    Done Rector code UI

---
+++ forntend/admin-page/layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <nav
+      class="fixed w-full z-40 flex justify-between bg-white items-center max-w-full h-10"
+    >
+      <div class="titleName ml-64" v-if="isLogin">
+        <p class="text-lg">
+          Welcome Admin : <span style="color: #ffc700">{{ username }}</span>
+        </p>
+      </div>
+      <div
+        class="exit order-last mr-16 flex-none cursor-pointer"
+        v-if="isLogin"
+      >
+        <img src="~/assets/ADMIN/logout_black_24dp.svg" alt="" />
+      </div>
+    </nav>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLogin: true,
+      username: 'test',
+    }
+  },
+}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  background: #dbdbdb;
+}
+</style>
---
+++ forntend/admin-page/pages/order.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <main class="flex flex-wrap relative">
+      <adminSide :isLogin="true" />
+      <section class="mt-10 w-5/6">
+        <adminDashBoard class="mt-2 mb-4 mx-2" />
+        <div class="hero bg-white mx-auto w-11/12">
+          <div class="top-bar flex justify-between">
+            <form class="w-full max-w-sm">
+              <div class="flex items-center py-2 px-3">
+                <img src="~/assets/ADMIN/search_black_24dp.svg" alt="" />
+                <input
+                  class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                  type="text"
+                  placeholder="Search"
+                />
+                <img
+                  src="~/assets/ADMIN/Filter.svg"
+                  alt=""
+                  class="mt-1 cursor-pointer"
+                />
+              </div>
+            </form>
+          </div>
+          <div class="tableBook">
+            <table class="table-fixed w-full">
+              <thead>
+                <tr>
+                  <th class="px-4 py-2 w-1/12">ID</th>
+                  <th class="px-4 py-2 w-1/12">Customer id</th>
+                  <th class="px-4 py-2 w-1/12">Date</th>
+                  <th class="px-4 py-2 w-1/12">amount</th>
+                  <th class="px-4 py-2 w-2/12">price</th>
+                  <th class="px-4 py-2 w-1/12">payment</th>
+                  <th class="px-4 py-2 w-1/12">delivery date</th>
+                  <th class="px-4 py-2 w-1/12">admin</th>
+                  <th class="px-4 py-2 w-3/12">Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(i, index) in 20" :key="index">
+                  <td class="border px-4 py-2">1</td>
+                  <td class="border px-4 py-2">2</td>
+                  <td class="border px-4 py-2">20/01/12</td>
+                  <td class="border px-4 py-2">13</td>
+                  <td class="border px-4 py-2 truncate">2000 Bath</td>
+                  <td class="border px-4 py-2">
+                    <span class="text-green-600"> Yes </span>
+                    <span class="text-red-600"> No </span>
+                  </td>
+                  <td class="border px-4 py-2">-</td>
+                  <td class="border px-4 py-2">-</td>
+                  <td class="border px-4 py-2">
+                    <div class="icon text-center">
+                      <img
+                        src="~/assets/ADMIN/Edit.svg"
+                        alt=""
+                        class="inline mx-3"
+                      />
+                      <img
+                        src="~/assets/ADMIN/Group.svg"
+                        alt=""
+                        class="inline mx-3"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+}
+</script>
+
+<style></style>
