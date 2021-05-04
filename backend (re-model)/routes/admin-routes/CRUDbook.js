cb9563d2ae363b1a9dd30010c65aa97e2962e674
Nipas Wongnitipat <50570367+TakaiKung@users.noreply.github.com>
2021-05-05 01:16:08 +0700

    d

--- backend (re-model)/routes/admin-routes/CRUDbook.js
+++ backend (re-model)/routes/admin-routes/CRUDbook.js
@@ -48,10 +48,10 @@ router.post('/addBook', passport.authenticate('jwt', { session: false }), upload
                 return res.json({ massage: "Duplicate Book" })
             }
             else {
-                const insertBook = `INSERT INTO BOOK SELECT 0, ?, ?, ?, ?, ?, ?, ? FROM DUAL WHERE NOT EXISTS(  SELECT book_name FROM BOOK WHERE book_name=? LIMIT 1 ); SELECT book_id FROM BOOK WHERE book_name=?`
+                const insertBook = `INSERT INTO BOOK SELECT 0, ?, ?, ?, ?, ?, ?, ? FROM DUAL WHERE NOT EXISTS(  SELECT book_name FROM BOOK WHERE book_name=? LIMIT 1 );`
 
 
-                const book = await connection.query(insertBook, [name, year, price, amount, book_image, des, pop, name, name])
+                const book = await connection.query(insertBook, [name, year, price, amount, book_image, des, pop, name])
                 console.log(book[0][0]);
                 console.log(book[0]);
                 console.log(book);
