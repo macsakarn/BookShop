6acdf0f72216f7f8fc0a905dc42648360a372760
Takai <nipas2013@gmail.com>
2021-05-05 16:50:23 +0700

    add validate user login

--- backend (re-model)/library/validateModule.js
+++ backend (re-model)/library/validateModule.js
@@ -18,4 +18,41 @@ function validateOrder ( data ) {
    
 }
 
+function validateUserRegister ( data, adata ) {
+    console.log("Validating......")
+    const v = new validator();
+    // const test = {
+    //     customer_email : "nipas2013@gmail.com",
+    //     customer_fname : "Nipas",
+    //     customer_lname : "Wongnitipat",
+    //     customer_adress : "ABCDEFG",
+    //     customer_tel  : '0927593986',
+    //     account : {
+    //         username : "takai",
+    //         password : "Takai02321560"
+    //     }
+    // }
+
+    const schema = {
+        email  : { type : "email" },
+        fname  : { type: "string", min: 3, max: 255 },
+        lname  : { type: "string", min: 3, max: 255 },
+        adress : { type: "string", min: 3, max: 255 },
+        tel    : { type :"string", numeric :true},
+    }
+
+    const aSchema = {
+        username : {type : "string", min: 3, max: 255},
+        password : {type : "string", pattern : "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])", min :8 , max:255 }
+    }
+
+    const result = v.validate(data, schema);
+    const aResult = v.validate(adata, aSchema);
+
+    console.log(`Validate result : ${result} , ${aResult}`);
+
+    return {customer_result : result, account_result : aResult};
+}
+
 module.exports.validateOrder = validateOrder;
+module.exports.validateUserRegister = validateUserRegister;
